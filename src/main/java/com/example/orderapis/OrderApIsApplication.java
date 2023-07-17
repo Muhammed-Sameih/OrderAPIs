7414a819dcdd480c5b8fe0da961d8a1894edbdf7
Muhammed Sameih <muhamedsamih748@gmail.com>
2023-07-17 20:24:02 +0300

    updating pom file

---
+++ src/main/java/com/example/orderapis/OrderApIsApplication.java
@@ -0,0 +1,13 @@
+package com.example.orderapis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApIsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderApIsApplication.class, args);
+	}
+
+}
