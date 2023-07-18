920186da8a30edaaee1f4f4989945c1513c7a4aa
Muhammed Sameih <muhamedsamih748@gmail.com>
2023-07-18 12:45:42 +0300

    creating order Item entity

--- src/main/java/com/example/orderapis/entity/OrderItem.java
+++ src/main/java/com/example/orderapis/entity/OrderItem.java
@@ -1,2 +1,31 @@
-package com.example.orderapis.entity;public class OrderItem {
+package com.example.orderapis.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "order_item")
+@Data
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_item_id")
+    private Long orderItemId;
+
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @Column(name = "product_id")
+    private Long productId;
+
+    @Column(name = "quantity")
+    private Long quantity;
+
+    @Column(name = "price")
+    private BigDecimal price;
+
 }
---
+++ src/main/resources/db/migration/V1__Create_Order_Item_Tables.sql
@@ -0,0 +1,21 @@
+DROP TABLE IF EXISTS orders;
+DROP TABLE IF EXISTS order_item;
+
+-- Create the Order table
+CREATE TABLE orders (
+                        order_id SERIAL PRIMARY KEY,
+                        customer_id INTEGER,
+                        coupon_id INTEGER,
+                        order_date TIMESTAMP,
+                        total_amount DECIMAL(10, 2),
+                        status VARCHAR(20)
+);
+
+-- Create the OrderItem table
+CREATE TABLE order_item (
+                            order_item_id SERIAL PRIMARY KEY,
+                            order_id INTEGER REFERENCES orders (order_id),
+                            product_id INTEGER,
+                            quantity INTEGER,
+                            price DECIMAL(10, 2)
+);
